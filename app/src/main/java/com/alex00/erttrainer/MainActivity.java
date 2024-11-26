ea9a13ffd509cb87045e1aed7a759ea238d5fa26
Guadalupe Alejandro Perez Camacho <alexnengveerav@gmail.com>
2024-11-26 15:26:12 -0600

    actualización del gradle a la version 8.9 y nuevas versiones alternativas de la activity_quiz creadas

--- app/src/main/java/com/alex00/erttrainer/MainActivity.java
+++ app/src/main/java/com/alex00/erttrainer/MainActivity.java
@@ -1,11 +1,10 @@
 package com.alex00.erttrainer;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ScrollView;
 import android.widget.TextView;
 
 import com.android.volley.Request;
